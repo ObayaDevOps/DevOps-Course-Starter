00a8d0eed7ea0148ffac1babc3fd764a2b6173bf
Obaya Dralega <obayajamesdralega@gmail.com>
2020-10-19 20:54:13 +0100

    Modified app to show index page template

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-
+# Need to make the page display the template
 @app.route('/')
 def index():
-    return 'Hello World!'
+    # todo_items=['item1']
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
