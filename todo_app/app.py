b837de0f10bd3ec1f0b8374dbd7be39a4c191e1f
Obaya Dralega <obayajamesdralega@gmail.com>
2020-10-19 21:39:22 +0100

    Display the list of todo items from session

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template
+# import .data.session_items
+from .data.session_items import get_items, add_item
+
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-# Need to make the page display the template
+# Now need to retreive the items fron the session file and pass them to the index.html
 @app.route('/')
 def index():
-    # todo_items=['item1']
-    return render_template('index.html')
+    return render_template('index.html', todo_items=get_items())
 
 
 if __name__ == '__main__':
